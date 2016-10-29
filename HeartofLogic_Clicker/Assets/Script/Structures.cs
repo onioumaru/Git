d078dfb3e2e38847b0fc0d033d9cb5a47287e038
golden_duck <golden_duck2@hotmail.com>
2016-10-30 06:17:55 +0900

    10/30

---
+++ HeartofLogic_Clicker/Assets/Script/Structures.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Structures : MonoBehaviour {
+	public float ether = 0f;
+
+	public float earth = 0f;
+	public float spirit = 0f;
+
+	public float fuel = 0f;
+	public float metal = 0f;
+	public float chemical = 0f;
+
+
+
+}
---
+++ HeartofLogic_Clicker/Assets/Script/mainClickCollider.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class mainClickCollider : MonoBehaviour {
+	public Text _testcountText;
+
+	private float testClickcount;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseDown(){
+		//Debug.Log ("click");
+		testClickcount += 1f;
+		_testcountText.text = testClickcount.ToString ();
+
+	}
+
+
+
+
+
+
+}
